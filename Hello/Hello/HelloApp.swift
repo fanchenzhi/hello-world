8d734eec0aeeafad364c10d1c9198d568374a5a8
Fred <583872242@qq.com>
2024-11-08 15:57:22 +0800

    基础框架

---
+++ Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Fred Fan on 2024/11/8.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello/HelloTests/HelloTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTests.swift
+//  HelloTests
+//
+//  Created by Fred Fan on 2024/11/8.
+//
+
+import Testing
+@testable import Hello
+
+struct HelloTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
